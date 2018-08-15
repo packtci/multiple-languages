5883ea0c0177bc0f5b9072488f8ff33cb0671d1f
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-08-15 19:15:45 -0400

    Add deployment script to repo.

---
+++ scripts/deploy.go
@@ -0,0 +1,88 @@
+package deploy
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	tagName         = flag.String("tagName", "", "Please provide a tag name for the release")
+	targetCommitish = flag.String("targetCommitish", "", "Please provide a targetCommitish value namely master")
+	name            = flag.String("name", "", "A name for the Github Release")
+	body            = flag.String("body", "", "Provide a description of the release")
+)
+
+func checkArgs() {
+	if *tagName == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *targetCommitish == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *name == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *body == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	checkArgs()
+
+	body := struct {
+		TagName         string `json:"tag_name"`
+		TargetCommitish string `json:"target_commitish"`
+		Name            string `json:"name"`
+		Body            string `json:"body"`
+		Draft           bool   `json:"draft"`
+		Prerelease      bool   `json:"prerelease"`
+	}{
+		*tagName,
+		*targetCommitish,
+		*name,
+		*body,
+		false,
+		false,
+	}
+
+	postBody, err := json.Marshal(body)
+	if err != nil {
+		log.Fatalln("An issue occurred marshalling this data ", err)
+	}
+
+	client := http.Client{}
+
+	req, err := http.NewRequest(
+		"POST",
+		"https://api.github.com/repos/packci/multiple-languages/releases",
+		bytes.NewBuffer(postBody),
+	)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "token "+os.Getenv("PACKTCI_PERSONAL_TOKEN"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["url"])
+}
